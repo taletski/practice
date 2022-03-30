1bbf8b0bac029edc80f48afee14d3ed540ab84b7
taletski <kirill@taletski.com>
2022-03-30 11:06:04 +0300

    adds the first working version of firstBadVersion

--- src/leetcode/278-firstBadVersion/firstBadVersion.ts
+++ src/leetcode/278-firstBadVersion/firstBadVersion.ts
@@ -1,5 +1,28 @@
+/**
+ * Uses binary search, `O(n*log(n))`
+ */
 export const firstBadVersion =
   (isBadVersion: (version: number) => boolean) =>
   (latestVersion: number): number => {
-    return 0;
+    let left = 0;
+    let right = latestVersion;
+
+    let counter = 0;
+
+    while (left < right) {
+      const middle = Math.round((right + left) / 2);
+
+      if (isBadVersion(middle)) {
+        right = middle;
+      } else {
+        left = middle + 1;
+      }
+
+      if (counter++ > 100)
+        throw new Error(
+          `firstBadVersion stuck with left=${left}, middle=${middle}, right=${right}`
+        );
+    }
+
+    return left;
   };
